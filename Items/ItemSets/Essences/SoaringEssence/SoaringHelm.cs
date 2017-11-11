d0479f35deec6e06cbf4e39ca61a834024a02881
Paradiscal <Paradiscal@outlook.com>
2017-11-11 12:23:51 +0000

    soaring weeb
    zoro needs to do setbonus and someone needs to sprite the icons

---
+++ Items/ItemSets/Essences/SoaringEssence/SoaringHelm.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ForgottenMemories.Items.ItemSets.Essences.SoaringEssence
+{
+	[AutoloadEquip(EquipType.Head)]
+	public class SoaringHelm : ModItem
+	{
+
+
+		public override void SetDefaults()
+		{
+
+			item.width = 18;
+			item.height = 18;
+			item.value = 35000;
+			item.rare = 2;
+			item.defense = 4;
+		}
+
+		public override void SetStaticDefaults()
+		{
+		  DisplayName.SetDefault("Soaring Cap");
+		  Tooltip.SetDefault("5% increased magic damage and critical strike chance");
+		}
+
+
+		public override bool IsArmorSet(Item head, Item body, Item legs)
+		{
+			return body.type == mod.ItemType("SoaringChest") && legs.type == mod.ItemType("SoaringGreaves");
+		}
+
+        public override void UpdateEquip(Player player)
+        {
+            player.magicDamage += 0.05f;
+			player.magicCrit += 5;
+        }
+
+		public override void UpdateArmorSet(Player player)
+		{
+			player.setBonus = "Zoro is a weeb";
+		}
+
+        public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(null,"SoaringEnergy", 16);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
