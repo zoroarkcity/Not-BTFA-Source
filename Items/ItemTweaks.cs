c2262a039933ad78cd2b5eaa9fbe32e6aef4e91b
gratgaisdead <gratga2807@gmail.com>
2017-11-30 17:48:01 -0200

    tfw more sprites and no zoro code

---
+++ Items/ItemTweaks.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ForgottenMemories.Items
+{
+	public class ItemTweaks : GlobalItem
+	{
+		public override void SetDefaults(Item item)
+		{
+			switch(item.type)
+			{
+				case ItemID.Acorn:
+					item.ranged = true;
+					item.ammo = item.type;
+					item.consumable = true;
+					item.maxStack = 999;
+					break;
+			}
+		}
+	}
+}
