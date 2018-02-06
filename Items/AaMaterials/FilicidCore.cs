ec5e0074d571c389a3faa9e73d7135ff373a39b4
zoroarkcity <zoroarkcity2002@gmail.com>
2018-02-06 07:58:58 -0800

    ur

---
+++ Items/AaMaterials/FilicidCore.cs
@@ -0,0 +1,36 @@
+using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+
+namespace ForgottenMemories.Items.AaMaterials
+{
+	public class FilicidCore : ModItem
+	{
+		public override void SetDefaults()
+		{
+
+			item.width = 10;
+			item.height = 10;
+			item.noMelee = true; 
+			item.value = 80;
+			item.rare = 1;
+			item.maxStack = 999;
+		}
+		
+		public override void Update(ref float gravity, ref float maxFallSpeed)
+		{
+			maxFallSpeed = 0f;
+		}
+
+    public override void SetStaticDefaults()
+    {
+      DisplayName.SetDefault("Filicid Core");
+      Tooltip.SetDefault("'Tainted by the Red Moon'");
+	  Main.RegisterItemAnimation(item.type, new DrawAnimationVertical(4, 4));
+    }
+
+	}
+}
