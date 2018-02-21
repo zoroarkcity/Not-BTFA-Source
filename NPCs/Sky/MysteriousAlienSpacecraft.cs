cbe8a61dd5c45b9243370b231f85f622da7fcd92
Paradiscal <Paradiscal@outlook.com>
2018-02-21 23:10:21 +0000

    even more gamer epic

--- NPCs/Sky/MysteriousAlienSpacecraft.cs
+++ NPCs/Sky/MysteriousAlienSpacecraft.cs
@@ -47,7 +47,7 @@ namespace ForgottenMemories.NPCs.Sky
 		
 		public override void NPCLoot()
 		{
-			if(Main.rand.Next(4) == 0)
+			if(Main.rand.Next(2) == 0)
 			{
 				Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("DivineBolt"));
 			}
