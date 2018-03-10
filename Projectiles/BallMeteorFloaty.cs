814beae6d327468de734deecab91693160b8f230
terrynmuse <37078944+terrynmuse@users.noreply.github.com>
2018-03-09 21:13:02 -0500

    new titan rock projs

---
+++ Projectiles/BallMeteorFloaty.cs
@@ -0,0 +1,130 @@
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.GameContent;
+using Terraria.ModLoader;
+
+namespace ForgottenMemories.Projectiles
+{
+	public class BallMeteorFloaty : ModProjectile
+	{
+        public bool becomeRealBall = false;
+        
+        public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Ball Meteor");
+		}
+
+		public override void SetDefaults()
+		{
+            projectile.width = 36;
+            projectile.height = 34;
+            projectile.aiStyle = -1;
+            //projectile.melee = true; //literally why
+            projectile.hostile = true;
+            projectile.penetrate = 1;
+            projectile.tileCollide = false;
+            projectile.light = 0.5f;
+            projectile.extraUpdates = 3;
+		}
+
+        public void BaseAI() //ai stolen from original ball meteor
+        {
+            projectile.rotation += projectile.velocity.X / 2f;
+            Vector2 vector2 = projectile.Center + Vector2.Normalize(projectile.velocity) * 10f;
+            Dust dust1 = Main.dust[Dust.NewDust(projectile.position, projectile.width, projectile.height, 60, 0.0f, 0.0f, 0, new Color(), 1f)];
+            dust1.position = vector2;
+            dust1.velocity = projectile.velocity.RotatedBy(1.57079637050629, new Vector2()) * 0.33f + projectile.velocity / 4f;
+            dust1.position += projectile.velocity.RotatedBy(1.57079637050629, new Vector2());
+            dust1.fadeIn = 0.5f;
+            dust1.noGravity = true;
+            Dust dust2 = Main.dust[Dust.NewDust(projectile.position, projectile.width, projectile.height, 60, 0.0f, 0.0f, 0, new Color(), 1f)];
+            dust2.position = vector2;
+            dust2.velocity = projectile.velocity.RotatedBy(-1.57079637050629, new Vector2()) * 0.33f + projectile.velocity / 4f;
+            dust2.position += projectile.velocity.RotatedBy(-1.57079637050629, new Vector2());
+            dust2.fadeIn = 0.5f;
+            dust2.noGravity = true;
+            for (int index1 = 0; index1 < 1; ++index1)
+            {
+                int index2 = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 60, 0.0f, 0.0f, 0, new Color(), 1f);
+                Main.dust[index2].velocity *= 0.5f;
+                Main.dust[index2].scale *= 1.3f;
+                Main.dust[index2].fadeIn = 1f;
+                Main.dust[index2].noGravity = true;
+            }
+            //if (projectile.position.Y > (double)projectile.ai[1]) projectile.tileCollide = true;
+        }
+
+        public override void AI()
+        {
+            BaseAI();
+
+            //when spawned, decelerate to a near stop
+            if (projectile.velocity.Length() > 0.05f)
+            {
+                projectile.velocity *= 0.982f;
+            }
+            else
+            {
+                becomeRealBall = true;
+                projectile.Kill();
+            }
+        }
+
+        public override void Kill(int timeLeft)
+		{
+            Main.PlaySound(SoundID.Item89, projectile.position);
+            
+            //when comes to a stop, "this" rock shoots at nearest player
+            if (becomeRealBall)
+            {
+                int target = Player.FindClosest(projectile.Center, 0, 0);
+
+				Vector2 targetLocation = Main.player[target].Center;
+				if (Main.rand.Next(2) == 0) //may or may not lead the player
+					targetLocation += Main.player[target].velocity * 17f;
+
+                Vector2 direction = Vector2.Subtract(targetLocation, projectile.Center);
+                direction.Normalize();
+				Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y, direction.X * 6f, direction.Y * 6f, mod.ProjectileType("BallMeteor"), projectile.damage, 1, Main.myPlayer);
+
+                return;
+            }
+            
+            projectile.position.X += (float) (projectile.width / 2);
+			projectile.position.Y += (float) (projectile.height / 2);
+			projectile.width = (int) (128.0 * (double) projectile.scale);
+			projectile.height = (int) (128.0 * (double) projectile.scale);
+			projectile.position.X -= (float) (projectile.width / 2);
+			projectile.position.Y -= (float) (projectile.height / 2);
+			for (int index = 0; index < 8; ++index)
+			  Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 60, 0.0f, 0.0f, 100, new Color(), 1.5f);
+			for (int index1 = 0; index1 < 32; ++index1)
+			{
+			  int index2 = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 60, 0.0f, 0.0f, 100, new Color(), 2.5f);
+			  Main.dust[index2].noGravity = true;
+			  Main.dust[index2].velocity *= 3f;
+			  int index3 = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 60, 0.0f, 0.0f, 100, new Color(), 1.5f);
+			  Main.dust[index3].velocity *= 2f;
+			  Main.dust[index3].noGravity = true;
+			}
+			for (int index1 = 0; index1 < 2; ++index1)
+			{
+			  int index2 = Gore.NewGore(projectile.position + new Vector2((float) (projectile.width * Main.rand.Next(100)) / 100f, (float) (projectile.height * Main.rand.Next(100)) / 100f) - Vector2.One * 10f, new Vector2(), Main.rand.Next(61, 64), 1f);
+			  Main.gore[index2].velocity *= 0.3f;
+			  Main.gore[index2].velocity.X += (float) Main.rand.Next(-10, 11) * 0.05f;
+			  Main.gore[index2].velocity.Y += (float) Main.rand.Next(-10, 11) * 0.05f;
+			}
+			if (projectile.owner == Main.myPlayer)
+			{
+			  projectile.localAI[1] = -1f;
+			  projectile.maxPenetrate = 0;
+			  projectile.Damage();
+			}
+		}
+	}
+}
---
+++ Projectiles/TitanMarkShower.cs
@@ -0,0 +1,87 @@
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.GameContent;
+using Terraria.ModLoader;
+
+namespace ForgottenMemories.Projectiles
+{
+	public class TitanMarkShower : ModProjectile
+	{
+        public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Titan Mark Shower");
+			Main.projFrames[projectile.type] = 2;
+		}
+
+		public override void SetDefaults()
+		{
+			projectile.width = 102;
+            projectile.height = 102;
+			projectile.timeLeft = 200;
+			projectile.penetrate = -1;
+			projectile.hostile = true;
+		}
+
+        public override bool CanHitPlayer(Player target)
+        {
+            return false;
+        }
+
+		public void RainMeteors()
+		{
+			int type = mod.ProjectileType("BallMeteor");
+
+            for (int i = 0; i < 3; i++)
+            {
+                Vector2 offset = new Vector2((float) Main.rand.Next(-300, 301), (float) Main.rand.Next(-1000, -701));
+                Vector2 spawnPosition = projectile.Center + offset;
+                spawnPosition.Y -= (float) (100 * i);
+
+                Vector2 velocity = Vector2.Subtract(projectile.Center, spawnPosition);
+                if (velocity.Y < 0f) //always goes down
+                    velocity.Y *= -1f;
+                if (velocity.Y < 20f) //always at least 20
+                    velocity.Y = 20f;
+                velocity.Normalize();
+                velocity *= 8.5f;
+
+                float velX = velocity.X;
+                float velY = velocity.Y + (float) Main.rand.Next(-40, 41) * 0.02f;
+
+                Projectile.NewProjectile(spawnPosition.X, spawnPosition.Y, velX, velY, type, projectile.damage, 1, Main.myPlayer, 0f, projectile.Center.Y);
+            }
+		}
+
+		public override void AI()
+        {
+            if (projectile.timeLeft <= 60)
+            {
+				projectile.frame = 0;
+
+                if (projectile.timeLeft % 10 == 0)
+				{
+					RainMeteors();
+				}
+            }
+			else
+			{
+				Player target = Main.player[(int) projectile.ai[0]];
+				if (target.active && !target.dead)
+					projectile.Center = target.Center;
+				
+				if (projectile.timeLeft % 15 == 0)
+				{
+					if (projectile.frame == 0)
+						projectile.frame++;
+					else
+						projectile.frame--;
+				}
+			}
+		}
+	}
+}
