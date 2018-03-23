05c1f3d942fccfe39806936e8ebc3cdf33fb0ab0
terrynmuse <37078944+terrynmuse@users.noreply.github.com>
2018-03-23 18:36:08 -0400

    better buff arterius
    better explosive zits and zit aiming, fewer calculations kinda

--- NPCs/FaceOfInsanity/ExplosiveZitEnemy.cs
+++ NPCs/FaceOfInsanity/ExplosiveZitEnemy.cs
@@ -9,17 +9,18 @@ namespace ForgottenMemories.NPCs.FaceOfInsanity
 {
 	public class ExplosiveZitEnemy : ModNPC
 	{
-		public bool hasSpawned = false;
-		public const float gravity = 0.3f;
-		public const float time = 60f;
-		public float maxVelY = 9999f;
+        public bool hasSpawned = false;
+        public const float gravity = 0.3f;
+        //public const float time = 90f;
+        public const float halfTime = 45f;
+		//public float maxVelY = 9999f;
 		
 		public override void SetDefaults()
 		{
 			npc.width = 28;
 			npc.height = 32;
 			npc.friendly = false;
-			npc.damage = 50;
+			npc.damage = 83;
 			npc.defense = 0;
 			npc.lifeMax = 1;
 			npc.aiStyle = -1;
@@ -31,7 +32,7 @@ namespace ForgottenMemories.NPCs.FaceOfInsanity
 
 		public override void ScaleExpertStats(int numPlayers, float bossLifeScale)
 		{
-			npc.damage = 100;
+			npc.damage = 166;
 		}
 		
 		public override void SetStaticDefaults()
@@ -43,11 +44,13 @@ namespace ForgottenMemories.NPCs.FaceOfInsanity
 		{
 			if (!hasSpawned)
 			{
-				Player player = Main.player[npc.target];
+				/*Player player = Main.player[npc.target];
 				float xDistance = player.Center.X - npc.Center.X;
 				maxVelY = gravity * time / 2f;
 				npc.velocity.X = xDistance / time + Main.rand.Next(-2, 2);
-				npc.velocity.Y = -maxVelY + Main.rand.Next(-2, 2);
+				npc.velocity.Y = -maxVelY + Main.rand.Next(-2, 2);*/
+                npc.velocity.X = npc.ai[1];
+                npc.velocity.Y = npc.ai[2];
 
 				hasSpawned = true;
 			}
@@ -71,23 +74,23 @@ namespace ForgottenMemories.NPCs.FaceOfInsanity
 				npc.alpha -= 5;
 			}
 
-			if ((npc.velocity.X == 0 || npc.velocity.Y == 0) && npc.ai[0] > time / 2)
+			if ((npc.velocity.X == 0 || npc.velocity.Y == 0) && npc.ai[0] > halfTime)
 			{
 				npc.hide = true;
 				npc.life = 0;
-				int p = Projectile.NewProjectile(npc.Center.X, npc.Center.Y, 0, 0, mod.ProjectileType("ExplosiveZit"), npc.damage / 3, 1, Main.myPlayer, 0, 0);
+				int p = Projectile.NewProjectile(npc.Center.X, npc.Center.Y, 0, 0, mod.ProjectileType("ExplosiveZit"), npc.damage / 4, 1, Main.myPlayer, 0, 0);
 				Main.projectile[p].netUpdate = true;
 				Main.projectile[p].Kill();
 			}
 			npc.ai[0]++;
 
-			if (npc.velocity.Y < maxVelY)
-				npc.velocity.Y += gravity;
+			//if (npc.velocity.Y < npc.ai[1]) 
+			npc.velocity.Y += gravity;
 		}
 		
 		public override bool CheckDead()
 		{
-			int p = Projectile.NewProjectile(npc.Center.X, npc.Center.Y, 0, 0, mod.ProjectileType("ExplosiveZit"), npc.damage, 1, Main.myPlayer, 0, 0);
+			int p = Projectile.NewProjectile(npc.Center.X, npc.Center.Y, 0, 0, mod.ProjectileType("ExplosiveZit"), npc.damage / 4, 1, Main.myPlayer, 0, 0);
 			Main.projectile[p].netUpdate = true;
 			Main.projectile[p].Kill();
 
