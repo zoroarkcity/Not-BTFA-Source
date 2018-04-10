088fc532143cd4af3438484c4951b4c04f49cd2a
terrynmuse <37078944+terrynmuse@users.noreply.github.com>
2018-04-09 23:31:54 -0400

    pls sprite new debuff icons maybe i guess

---
+++ Buffs/BoilingBlood.cs
@@ -0,0 +1,34 @@
+using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace ForgottenMemories.Buffs
+{
+	public class BoilingBlood : ModBuff
+	{
+		public override void SetDefaults()
+		{
+			DisplayName.SetDefault("Boiling Blood");
+			Description.SetDefault("Melting from within");
+			Main.debuff[Type] = true;
+            Main.pvpBuff[Type] = true;
+            Main.buffNoSave[Type] = true;
+            longerExpertDebuff = true;
+            canBeCleared = true;
+		}
+        
+        public override void Update(NPC npc, ref int buffIndex)
+        {
+            npc.GetGlobalNPC<BTFANPC>(mod).boilingBlood = true;
+        }
+
+		public override bool ReApply (NPC npc, int time, int buffIndex)
+		{
+			if (npc.GetGlobalNPC<BTFANPC>(mod).boilingBloodCounter < 140)
+				npc.GetGlobalNPC<BTFANPC>(mod).boilingBloodCounter += 7;
+
+			return false;
+		}
+	}
+}
---
+++ Buffs/Hematohidrosis.cs
@@ -0,0 +1,32 @@
+using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace ForgottenMemories.Buffs
+{
+	public class Hematohidrosis : ModBuff
+	{
+		public override void SetDefaults()
+		{
+			DisplayName.SetDefault("Hematohidrosis");
+			Description.SetDefault("Your blood is seeping away");
+			Main.debuff[Type] = true;
+            //Main.pvpBuff[Type] = true;
+            Main.buffNoSave[Type] = true;
+            longerExpertDebuff = false;
+            canBeCleared = true;
+		}
+        
+        public override void Update(NPC npc, ref int buffIndex)
+        {
+            if (Main.rand.Next(10) == 0)
+            {
+                Vector2 position = npc.position + new Vector2(Main.rand.Next(npc.width), Main.rand.Next(npc.height));
+				int p = Projectile.NewProjectile(position.X, position.Y, 0, 0, mod.ProjectileType("SeepingBlood"), 20, 0, Main.myPlayer, npc.whoAmI);
+				Main.projectile[p].scale = 0.5f + (float) Main.rand.Next(76) / 100;
+            }
+            Lighting.AddLight(npc.position, 0.5f, 0, 0);
+        }
+	}
+}
