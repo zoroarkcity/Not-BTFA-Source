3ca69badd2de13ba0f9130de768ca9964c83391d
gratgaisdead <gratga2807@gmail.com>
2018-04-14 20:43:53 -0300

    boss checklist fix probably

--- ForgottenMemories.cs
+++ ForgottenMemories.cs
@@ -98,11 +98,11 @@ namespace ForgottenMemories
             {
                 // To include a description JK MEME TAG:
 				bossChecklist.Call("AddBossWithInfo", "Acheron", 5.5f, (Func<bool>)(() => TGEMWorld.downedAcheron), "Use a [i:" + ItemType("Unstable Wisp") + "] in the Underworld");
-				bossChecklist.Call("AddBossWithInfo", "Magnoliac", 4.1f, (Func<bool>)(() => TGEMWorld.downedMag), "Summon the forest's army using [i:" + ItemType("AncientLog") + "]");
-				bossChecklist.Call("AddEventWithInfo", "Forest's Army", 4.1f, (Func<bool>)(() => TGEMWorld.downedForestInvasion), "Summon the forest's army using [i:" + ItemType("AncientLog") + "]");
+				bossChecklist.Call("AddBossWithInfo", "Magnoliac", 5.1f, (Func<bool>)(() => TGEMWorld.downedMag), "Summon the forest's army using [i:" + ItemType("AncientLog") + "]");
+				bossChecklist.Call("AddEventWithInfo", "Forest's Army", 5.1f, (Func<bool>)(() => TGEMWorld.downedForestInvasion), "Summon the forest's army using [i:" + ItemType("AncientLog") + "]");
                 bossChecklist.Call("AddBossWithInfo", "Ghastly Ent", 9.4f, (Func<bool>)(() => TGEMWorld.downedGhastlyEnt), "Summon the forest's army using [i:" + ItemType("AncientLog") + "] and defeat it during hardmode");
 				bossChecklist.Call("AddBossWithInfo", "Arterius", 6.3f, (Func<bool>)(() => TGEMWorld.downedArterius), "Use a [i:" + ItemType("BloodClot") + "] at night");
-				bossChecklist.Call("AddBossWithInfo", "Titan Rock", 6.9f, (Func<bool>)(() => TGEMWorld.downedTitanRock), "Use a [i:" + ItemType("anomalydetector") + "]");
+				bossChecklist.Call("AddBossWithInfo", "Titan Rock", 7.1f, (Func<bool>)(() => TGEMWorld.downedTitanRock), "Use a [i:" + ItemType("anomalydetector") + "]");
 			}
 
             Mod btfaSongs = ModLoader.GetMod("BTFASongs");
