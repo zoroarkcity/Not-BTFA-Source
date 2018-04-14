dec6ed9f91179e0bae608e7ae6afe2f88e71fc56
terrynmuse <37078944+terrynmuse@users.noreply.github.com>
2018-04-14 00:03:58 -0400

    redoing arte weps (sal cute)

--- Items/ItemSets/Arterius/BloodLeech.cs
+++ Items/ItemSets/Arterius/BloodLeech.cs
@@ -11,7 +11,7 @@ namespace ForgottenMemories.Items.ItemSets.Arterius
 	{
 		public override void SetDefaults()
 		{
-			item.damage = 64;
+			item.damage = 44;
 			item.thrown = true;
 			item.width = 44;
 			item.height = 20;
@@ -19,15 +19,15 @@ namespace ForgottenMemories.Items.ItemSets.Arterius
 			item.useStyle = 1;
 			item.noMelee = true;
 			item.noUseGraphic = true;
-			item.knockBack = 2;
-			item.useTime = 11;
-			item.useAnimation = 11;
+			item.knockBack = 2.4f;
+			item.useTime = 12;
+			item.useAnimation = 12;
 			item.value = 7500;
 			item.rare = 4;
 			item.UseSound = SoundID.Item1;
 			item.autoReuse = true;
 			item.shoot = mod.ProjectileType("BloodLeech");
-			item.shootSpeed = 12.4f;
+			item.shootSpeed = 14f;
 			item.consumable = true;
 			item.maxStack = 999;
 		}
--- Items/ItemSets/Arterius/Scabular.cs
+++ Items/ItemSets/Arterius/Scabular.cs
@@ -13,19 +13,19 @@ namespace ForgottenMemories.Items.ItemSets.Arterius
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Scabular");
-			Tooltip.SetDefault("Casts blood balls that deal increasing damage over time\nEnemies that die while debuffed will explode into boiling blood and infect nearby enemies");
+			Tooltip.SetDefault("Casts blood balls that inflict a plethora of curses upon enemies");
 		}
 		
 		public override void SetDefaults()
 		{
-			item.damage = 59;
+			item.damage = 34;
 			item.noMelee = true;
 			item.magic = true;
-			item.mana = 14;
+			item.mana = 24;
 			item.width = 40;
 			item.height = 40;
-			item.useTime = 21;
-			item.useAnimation = 21;
+			item.useTime = 35;
+			item.useAnimation = 35;
 			item.useStyle = 1;
 			item.shoot = mod.ProjectileType("BloodBall");
 			item.UseSound = SoundID.Item8;
@@ -34,6 +34,14 @@ namespace ForgottenMemories.Items.ItemSets.Arterius
 			item.rare = 4;
 			item.autoReuse = true;
 			item.shootSpeed = 11.8f;
+		}
+
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+			Projectile.NewProjectile(position.X, position.Y, speedX, speedY, type, damage, knockBack, player.whoAmI);
+			Projectile.NewProjectile(position.X, position.Y, speedX + Main.rand.Next(-24, 25) * 0.14f, speedY + Main.rand.Next(-24, 25) * 0.14f, type, damage, knockBack, player.whoAmI, 1f);
+			Projectile.NewProjectile(position.X, position.Y, speedX + Main.rand.Next(-24, 25) * 0.14f, speedY + Main.rand.Next(-24, 25) * 0.14f, type, damage, knockBack, player.whoAmI, 2f);
+			return false;
 		}
 	}
 }
