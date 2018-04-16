78d52c9f6c674c4af7c457926274052724b739d1
terrynmuse <37078944+terrynmuse@users.noreply.github.com>
2018-04-16 18:51:39 -0400

    dust tweak??

--- Projectiles/ArteriusWep/BloodBall.cs
+++ Projectiles/ArteriusWep/BloodBall.cs
@@ -31,9 +31,9 @@ namespace ForgottenMemories.Projectiles.ArteriusWep
 				float num1 = projectile.velocity.X / 3f * (float) index1;
 				float num2 = projectile.velocity.Y / 3f * (float) index1;
 				int num3 = 4;
-				int index2 = Dust.NewDust(new Vector2(projectile.position.X + (float) num3, projectile.position.Y + (float) num3), projectile.width - num3 * 2, projectile.height - num3 * 2, mod.DustType("BloodDust2"), 0.0f, 0.0f, 0, default(Color), 1f);
+				int index2 = Dust.NewDust(new Vector2(projectile.position.X + (float) num3, projectile.position.Y + (float) num3), projectile.width - num3 * 2, projectile.height - num3 * 2, mod.DustType("BloodDust2"), 0.0f, 0.0f, 0, default(Color), 0.6f);
 				Main.dust[index2].noGravity = true;
-				Main.dust[index2].scale = 1.6f;
+				Main.dust[index2].scale = 1.4f;
 				Main.dust[index2].velocity *= 0.1f;
 				Main.dust[index2].velocity += projectile.velocity * 0.1f;
 				Main.dust[index2].position.X -= num1;
@@ -42,7 +42,7 @@ namespace ForgottenMemories.Projectiles.ArteriusWep
 			if (Main.rand.Next(2) == 0)
 			{
 				int num = 4;
-				int index = Dust.NewDust(new Vector2(projectile.position.X + (float) num, projectile.position.Y + (float) num), projectile.width - num * 2, projectile.height - num * 2, 60, 0.0f, 0.0f, 0, default(Color), 1f);
+				int index = Dust.NewDust(new Vector2(projectile.position.X + (float) num, projectile.position.Y + (float) num), projectile.width - num * 2, projectile.height - num * 2, 60, 0.0f, 0.0f, 0, default(Color), 0.6f);
 				Main.dust[index].velocity *= 0.25f;
 				Main.dust[index].velocity += projectile.velocity * 0.5f;
 				Main.dust[index].noGravity = true;
