c8e2f496fe050b91d27a9ffa9b70848b27faeba9
terrynmuse <37078944+terrynmuse@users.noreply.github.com>
2018-04-16 20:20:01 -0400

    adjusting cosmorock set bonus

--- Buffs/CosmicGift.cs
+++ Buffs/CosmicGift.cs
@@ -10,27 +10,32 @@ using Terraria.ModLoader;
 
 namespace ForgottenMemories.Buffs
 {
-	public class CosmicBoon : ModBuff
+	public class CosmicGift : ModBuff
 	{
 		public override void SetDefaults()
 		{
-			DisplayName.SetDefault("Cosmic Boon");
-			Description.SetDefault("Defensive stats increased, damage and movement speed decreased");
+			DisplayName.SetDefault("Cosmic Gift");
+			Description.SetDefault("Damage and movement speed increased, defensive stats decreased");
 			Main.buffNoTimeDisplay[Type] = true;
 		}
 		
 		public override void Update(Player player, ref int buffIndex)
 		{
-			player.statDefense += 12;
-			player.endurance += 0.1f;
-			player.lifeRegen += 3;
+			player.statDefense -= 12;
+			player.endurance -= 0.1f;
+			player.lifeRegen -= 3;
 			
-			player.magicDamage -= 0.1f;
-			player.meleeDamage -= 0.1f;
-			player.rangedDamage -= 0.1f;
-			player.minionDamage -= 0.1f;
-			player.thrownDamage -= 0.1f;
-			player.moveSpeed -= 0.3f;
+			player.magicDamage += 0.15f;
+			player.meleeDamage += 0.15f;
+			player.rangedDamage += 0.15f;
+			player.minionDamage += 0.15f;
+			player.thrownDamage += 0.15f;
+			player.moveSpeed += 0.3f;
+
+			player.meleeCrit += 10;
+			player.rangedCrit += 10;
+			player.magicCrit += 10;
+			player.thrownCrit += 10;
 			
 			if (Main.rand.Next(5) == 0)
 			{
@@ -42,9 +47,9 @@ namespace ForgottenMemories.Buffs
 				vector.Normalize();
 				vector *= (float)Main.rand.Next(50, 100) * 0.04f;
 				Main.dust[num5].velocity = vector;
-				vector.Normalize();
-				vector *= 34f;
-				Main.dust[num5].position = player.Center - vector;
+				//vector.Normalize();
+				//vector *= 34f;
+				Main.dust[num5].position = player.Center;
 			}
 		}
 	}
