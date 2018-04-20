be26ce65c08c4beaab4fe04beb6c710cf2b6d45a
terrynmuse <37078944+terrynmuse@users.noreply.github.com>
2018-04-19 21:21:40 -0400

    BLIGHTGASM (new chakram)

--- Projectiles/BChakramContact.cs
+++ Projectiles/BChakramContact.cs
@@ -14,13 +14,12 @@ namespace ForgottenMemories.Projectiles
 			projectile.height = 30;
 			projectile.aiStyle = -1;
 			projectile.friendly = true;
-			projectile.melee = true;
+			projectile.thrown = true;
 			projectile.penetrate = -1;
-			projectile.timeLeft = 5;
-			projectile.light = 0.5f;
+			projectile.extraUpdates = 2;
+            projectile.timeLeft = 600;
 			projectile.tileCollide = false;
-			projectile.scale = 1f;
-			projectile.alpha = 155;
+			projectile.hide = true;
 		}
 		
 		public override void SetStaticDefaults()
@@ -30,12 +29,48 @@ namespace ForgottenMemories.Projectiles
 		
 		public override void AI()
 		{
-			projectile.alpha += 20;
+			Projectile parent = Main.projectile[(int) projectile.ai[0]];
+			if (!parent.active || parent.type != mod.ProjectileType("BlightedChakram2"))
+			{
+				projectile.Kill();
+				return;
+			}
+			projectile.Center = parent.Center;
+		}
+
+		public override void Kill (int timeLeft)
+		{
+			Projectile parent = Main.projectile[(int) projectile.ai[0]];
+			if (parent.active && parent.type == mod.ProjectileType("BlightedChakram2"))
+			{
+				parent.Kill();
+			}
 		}
 
 		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
 		{
-			target.AddBuff(mod.BuffType("BlightFlame"), 180, false);
-		}		
+			target.immune[projectile.owner] = 5;
+			target.GetGlobalNPC<BTFANPC>(mod).blightChakramHits++;
+			if (target.GetGlobalNPC<BTFANPC>(mod).blightChakramHits == 4)
+			{
+				target.GetGlobalNPC<BTFANPC>(mod).blightChakramHits = 0;
+				target.immune[projectile.owner] = 0;
+				
+				for (int i = 0; i < 5; i++)
+				{
+					float posX = target.position.X + Main.rand.Next(target.width);
+					float posY = target.position.Y + Main.rand.Next(target.height);
+					int p = Projectile.NewProjectile(posX, posY, 0, 0, mod.ProjectileType("BlightBoomRange"), damage * 2, knockback * 1.5f, projectile.owner);
+					Main.projectile[p].ranged = false;
+					Main.projectile[p].thrown = true;
+					Main.projectile[p].scale = 1f + Main.rand.Next(-75, 51) / 100f;
+					Main.projectile[p].Damage();
+				}
+
+				Main.PlaySound(SoundID.Item20, target.position);
+			}
+			
+			target.AddBuff(mod.BuffType("BlightFlame"), 420, false);
+		}
 	}
 }
--- Projectiles/BlightedChakram2.cs
+++ Projectiles/BlightedChakram2.cs
@@ -7,11 +7,9 @@ using Terraria.ModLoader;
  
 namespace ForgottenMemories.Projectiles
 {
-    public class BlightedChakram : ModProjectile
+    public class BlightedChakram2 : ModProjectile
     {
-        public bool hasHitSomething = false;
-
-		public override void SetDefaults()
+        public override void SetDefaults()
         {
             projectile.width = 30;
             projectile.height = 30;
@@ -94,96 +92,14 @@ namespace ForgottenMemories.Projectiles
 			int dust = Dust.NewDust(projectile.position, projectile.width, projectile.height, 173, 0f, 0f); 
 			Main.dust[dust].scale = 1f;
 			Main.dust[dust].noGravity = true;
-			
-			if (projectile.velocity.X > 0)
-				projectile.spriteDirection = 1;
-            else
-				projectile.spriteDirection = -1;
-			
-			Vector2 target = projectile.position;
-
-            float minimumRange = 800f;
-
-            bool flag = false;
-            
-			if (!hasHitSomething && projectile.ai[0] != 1f && projectile.ai[1] > 10f)
-            {
-				for (int index = 0; index < 200; ++index)
-				{
-					NPC npc = Main.npc[index];
-					
-					if (npc.CanBeChasedBy((object) projectile, false))
-					{
-						Vector2 distance = npc.Center - projectile.Center;
-						float length = distance.Length();
-						if (length < minimumRange && Collision.CanHit(projectile.Center, 1, 1, npc.position, npc.width, npc.height) &&
-							Math.Abs(projectile.velocity.ToRotation() - distance.ToRotation()) < 2.35619449) //dont target enemies directly behind self
-						{
-							minimumRange = length;
-							target = npc.Center;
-							flag = true;
-						}
-					}
-				}
-            }
-
-            if (!flag)
-            {
-				target = projectile.Center + projectile.velocity * 100;
-
-				if (projectile.ai[1] >= 30)
-				{
-					projectile.ai[0] = 1f;
-					projectile.ai[1] = 0f;
-					projectile.netUpdate = true;
-				}
-            }
-
-            float num8 = 0.25f;
-            Vector2 vector2 = target - projectile.Center;
-            float num12 = 12f / vector2.Length();
-			vector2 *= num12;
-
-            if (projectile.velocity.X < vector2.X)
-            {
-				projectile.velocity.X += num8;
-				if (projectile.velocity.X < 0 && vector2.X > 0)
-				{
-					projectile.velocity.X += num8 * 2f;
-				}
-			}
-			else if (projectile.velocity.X > vector2.X)
-			{
-				projectile.velocity.X -= num8;
-				if (projectile.velocity.X > 0 && vector2.X < 0)
-				{
-					projectile.velocity.X -= num8 * 2f;
-				}
-			}
-			if (projectile.velocity.Y < vector2.Y)
-			{
-				projectile.velocity.Y += num8;
-				if (projectile.velocity.Y < 0 && vector2.Y > 0)
-				{
-					projectile.velocity.Y += num8 * 2f;
-				}
-			}
-			else if (projectile.velocity.Y > vector2.Y)
-			{
-				projectile.velocity.Y -= num8;
-				if (projectile.velocity.Y > 0 && vector2.Y < 0)
-				{
-					projectile.velocity.Y -= num8 * 2f;
-				}
-            }
-
-			//Projectile.NewProjectile(projectile.position.X, projectile.position.Y, 0f, 0f, mod.ProjectileType("BChakramContact"), projectile.damage, projectile.knockBack, projectile.owner);
 		}
 
-		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+		/*public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
 		{
 			hasHitSomething = true;
-			target.immune[projectile.owner] = 5;
+
+			//target.immune[projectile.owner] = 2;
+
 			target.GetGlobalNPC<BTFANPC>(mod).blightChakramHits++;
 			if (target.GetGlobalNPC<BTFANPC>(mod).blightChakramHits == 4)
 			{
@@ -205,6 +121,6 @@ namespace ForgottenMemories.Projectiles
 			}
 			
 			target.AddBuff(mod.BuffType("BlightFlame"), 420, false);
-		}
+		}*/
     }
 }
