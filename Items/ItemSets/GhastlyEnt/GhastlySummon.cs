354aad10ba8cf6b260460345db8f313873f2d315
Paradiscal <Paradiscal@outlook.com>
2018-05-22 19:14:01 +0100

    oops i did it again i pooped in my pants

--- Items/ItemSets/GhastlyEnt/GhastlySummon.cs
+++ Items/ItemSets/GhastlyEnt/GhastlySummon.cs
@@ -6,7 +6,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria.ModLoader;
 
-namespace ForgottenMemories.ItemSets.GhastlyEnt
+namespace ForgottenMemories.Items.ItemSets.GhastlyEnt
 {
     public class GhastlySummon : ModItem
     {
