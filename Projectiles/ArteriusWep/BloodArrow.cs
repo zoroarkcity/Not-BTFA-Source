aacdf796d39247a808627cfdc0b54e52016c76b1
Paradiscal <Paradiscal@outlook.com>
2018-05-27 02:11:22 +0100

    poop

--- Projectiles/ArteriusWep/BloodArrow.cs
+++ Projectiles/ArteriusWep/BloodArrow.cs
@@ -40,15 +40,9 @@ namespace ForgottenMemories.Projectiles.ArteriusWep
 			}
 		}
 
-		/*public override void Kill(int timeLeft)
+		public override void Kill(int timeLeft)
 		{
-			for (int i = 0; i < 3; i++)
-			{
-				float SpeedX = projectile.velocity.X * Main.rand.Next(20, 51) * 0.005f + Main.rand.Next(-20, 21) * 0.4f;
-				float SpeedY = -Math.Abs(projectile.velocity.Y) * Main.rand.Next(30, 51) * 0.01f + Main.rand.Next(-20, 21) * 0.4f;
-				int p = Projectile.NewProjectile(projectile.Center.X + SpeedX, projectile.Center.Y + SpeedY, SpeedX, SpeedY, mod.ProjectileType("BoilingBlood"), projectile.damage / 2, projectile.knockBack / 4, projectile.owner);
-				Main.projectile[p].scale = 0.75f + (float) Main.rand.Next(51) / 100;
-			}
-		}*/
+			Main.PlaySound(0, (int)projectile.position.X, (int)projectile.position.Y); //create a sound
+		}
 	}
 }	
