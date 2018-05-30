d9a6a4b62bbaabddfefe90f2c8d830d7e2f4b775
Paradiscal <Paradiscal@outlook.com>
2018-05-30 17:32:48 +0100

    poop

--- Items/Pet/Chicken/ChickenGiant.cs
+++ Items/Pet/Chicken/ChickenGiant.cs
@@ -12,6 +12,8 @@ namespace ForgottenMemories.Items.Pet.Chicken
 			Main.projFrames[projectile.type] = 13;
 			Main.projPet[projectile.type] = true;
 			projectile.tileCollide = true;
+			projectile.width = 50;
+			projectile.height = 54;
 		}
 
 		public override void SetDefaults()
